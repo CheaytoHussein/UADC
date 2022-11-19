58299edd73c4d792d0efef9d2e5912a381c849f1
CheaytoHussein <cheayto27@gmail.com>
2022-11-18 22:24:30 -0800

    Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,9 @@ const Home = (props) => {
       }}
     >
       <div className={props.isMobile ? "Mobile-Welcome" : "Desktop-Welcome"}>
-        <h3 className={props.isMobile ? "Mobile-Home-h3" : "Desktop-Home-h3"}>Welcome to the official website of UA's Developers' Club</h3>
+        <h3 className={props.isMobile ? "Mobile-Home-h3" : "Desktop-Home-h3"}>
+          Welcome to the official website of UA's Developers' Club
+        </h3>
         <p className={props.isMobile ? "Mobile-Home-p" : "Desktop-Home-p"}>
           This website was created to introduce people to our club, update all
           members of our future plans, the right people to contact, and so that
